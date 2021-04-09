327f79a7142dea091492dd3c7b854172d1f97426
KrzysztofBojarczuk <69596796+KrzysztofBojarczuk@users.noreply.github.com>
2021-04-09 13:32:34 +0200

    Add project files.

---
+++ 8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double mph;
+            double kph;
+
+            Console.WriteLine("KPH     MPH");
+            Console.WriteLine("-----------");
+
+            for (kph = 60; kph <= 130; kph = kph + 10)
+            {
+                mph = kph * 0.6214;
+
+                Console.WriteLine($"{kph}  {Math.Round(mph, 3)}");
+            }
+        }
+    }
+}
